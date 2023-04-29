f0ca8562156c9e5649a1f307ed042416dcf16a83
fillip1984 <fillip1984@gmail.com>
2023-04-29 13:25:53 -0400

    cleaning up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,8 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { useState } from "react";
-import { api } from "~/utils/api";
+
 const Home: NextPage = () => {
-  const [searchAdd, setSearchAdd] = useState("");
-
-  const ctx = api.useContext();
-
-  // const applicationQuery = api.applications.getAll();
-  const { data: applications } = api.applications.readAll.useQuery();
-  const { mutate: create } = api.applications.create.useMutation({
-    onSuccess: () => {
-      void ctx.applications.readAll.invalidate();
-    },
-  });
-  const { mutate: del } = api.applications.delete.useMutation({
-    onSuccess: () => {
-      void ctx.applications.readAll.invalidate();
-    },
-  });
-
   return (
     <>
       <Head>
