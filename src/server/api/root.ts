246608d34373e9b881c9ac94ef65be31109aa0bb
fillip1984 <fillip1984@gmail.com>
2023-04-28 23:29:48 -0400

    added in search add bar and working on making it function with trpc and prisma

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { applicationRouter } from "./routers/application";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  application: applicationRouter,
 });
 
 // export type definition of API
